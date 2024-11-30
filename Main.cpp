b23d82aa5444586d3fd77aa7d72235aeb1df8cfe
Crisitan Pena <aravier1pena@hotmail.com>
2024-11-30 13:02:32 -0800

    Comment Typo

--- Main.cpp
+++ Main.cpp
@@ -5,18 +5,15 @@ using namespace std;
 int main(){
     Rectangle r1(10, 5);
     cout << r1.toString() << endl;
-
-    // UNCOMMENT THE FOLLOWING LINES OF CODE
-    // ONCE YOU ARE DONE DEFINING YOUR CLASSES
-
-     Square s1(10);
-     cout << s1.toString() << endl;
-
-     Triangle t1(10, 5);
-     cout << t1.toString() << endl;
-
-     Circle c1(10);
-     cout << c1.toString() << endl;
+    
+    Square s1(10);
+    cout << s1.toString() << endl;
+    
+    Triangle t1(10, 5);
+    cout << t1.toString() << endl;
+    
+    Circle c1(10);
+    cout << c1.toString() << endl;
 
     return 0;
 }
